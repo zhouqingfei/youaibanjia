92d3544b7599817b2b510a6a4673d80a187f8a7f
cyberHz <dgcyb123@163.com>
2018-03-09 16:44:01 +0800

    页面初始化时获取用户的OPENID

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,12 @@
 //var API_HOST = 'https://yun.youai.cn/';
 //var API_HOST = 'http://localhost:8089/';
 var API_HOST = 'http://localhost:8089/';
+var APP_ID = 'wx97bb7f5346c34392';
+var APP_SECRET ='4fb1506033c571f13ff34fc415b9efc1';
 
 module.exports = {
-  host: API_HOST
+  host: API_HOST,
+  appId: APP_ID,
+  appSecret: APP_SECRET
+
 }
